22f4a814843b61896a56049ae940fdad66df0081
xxx <xiazhangyin@126.com>
2020-07-04 21:52:40 +0800

    my first go demo project

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"rsc.io/quote"
+	"foo/route"
+)
+
+func main() {
+	fmt.Println(quote.Hello())
+	f01.Ftest01()
+}
---
+++ route/fun1.go
@@ -0,0 +1,7 @@
+package f01
+
+import "fmt"
+
+func Ftest01(){
+    fmt.Println("tsssss")   
+}
